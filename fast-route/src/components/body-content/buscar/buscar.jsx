b6e06bc50952f54687e3c669d01e0b8a51fc83b0
erick albuquerque <ericgtxh@Hotmail.com>
2024-03-17 20:59:44 -0300

    quinto

--- fast-route/src/components/body-content/buscar/buscar.jsx
+++ fast-route/src/components/body-content/buscar/buscar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import icon_buscar from '../../../assets/busca_rotas.png'
+import icon_rotas from '../../../assets/busca_rotas.png'
+import icon_search from '../../../assets/search-icon.png'
 import './buscar.css'
 
 function Buscar() {
@@ -7,9 +8,29 @@ function Buscar() {
         <div className="desig-buscar">
             <div className="container-buscar">
                 <div className="container-title">
-                    <img className="icon-buscar" src={icon_buscar} alt="icon_buscar" />
+                    <img className="icon-buscar" src={icon_rotas} alt="icon_buscar" />
                     <h1 className="h1-text-buscar">Buscar Rotas</h1>
                 </div>
+                <form className="form">
+                    <div className="container-form">
+                        <label className="label-buscar">De onde você deseja partir?</label>
+                        <div className="div-input-buscar">
+                            <input className="input-buscar" />
+                        </div>
+                    </div>
+                    <div className="container-form">
+                        <label className="label-buscar">Para onde você deseja ir?</label>
+                        <div className="div-input-buscar">
+                            <input className="input-buscar" />
+                        </div>
+                    </div>
+                    <div className="submit-buscar">
+                        <div className="container-button-icon">
+                            <img className="icon-search" src={icon_search} alt="icon_buscar" />
+                            <li className="name-buscar">Buscar</li>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     )
